6bbe64abf47433caa5b1b945fdb2b81b80684762
Jspriddy <69536380+Jspriddy@users.noreply.github.com>
2022-01-20 16:52:49 -0600

    Add files via upload
    first commit

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Title      : Hello World!
+# Date       : 2022-01-20
+# Author     : "Jonathan Spring" <jonathandavidspring@gmail.com>
+# Version    : 1.0
+# Description: prints Hello, World!
+# Options    : None
+
+printf "%s\n" "Hello, World!" 
